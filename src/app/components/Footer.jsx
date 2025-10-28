9694f4cb655b0cb76493eb080367f0725bfa2e02
Vinicius Dilly <marcosvmdilly@gmail.com>
2025-10-28 09:38:32 -0300

    working on footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
   return (
     <>
       <div className='footer bg-black px-[2%] sm:px-[8%] pt-[50px] lg:pt-[90px]'>
-        <div className='footer-content'>
+        <div className='footer-content pb-5'>
           <div className='grid grid-cols-1 lg:grid-cols-12 gap-10'>
             <div className='lg:col-span-3'>
               <div className='text-white logo text-2xl uppercase font-semibold'>
@@ -27,6 +27,39 @@ export default function Footer() {
                 <FontAwesomeIcon icon={faXTwitter} className='text-[#ffffffb3] text-[20px] hover:text-white transition cursor-pointer' />
                 <FontAwesomeIcon icon={faFacebook} className='text-[#ffffffb3] text-[20px] hover:text-white transition cursor-pointer' />
                 <FontAwesomeIcon icon={faYoutube} className='text-[#ffffffb3] text-[20px] hover:text-white transition cursor-pointer' />
+              </div>
+            </div>
+
+            <div className='lg:col-span-9 grid grid-cols-1 md:grid-cols-12 gap-10'>
+
+              <div className='md:col-span-3 space-y-3'>
+                <h2 className='text-white text-2xl unbounded-font mb-6'>Page</h2>
+                <ul className='space-y-2 list-none'>
+                  <li><Link href='#' className='text-[#ffffffb3] hover:text-white transition-colors duration-300'>About Us</Link></li>
+                  <li><Link href='#' className='text-[#ffffffb3] hover:text-white transition-colors duration-300'>Services</Link></li>
+                  <li><Link href='#' className='text-[#ffffffb3] hover:text-white transition-colors duration-300'>FAQ</Link></li>
+                  <li><Link href='#' className='text-[#ffffffb3] hover:text-white transition-colors duration-300'>Contact Us</Link></li>
+                </ul>
+              </div>
+              <div className='md:col-span-3 space-y-3'>
+                <h2 className='text-white text-2xl unbounded-font mb-6'>Links</h2>
+                <ul className='space-y-2 list-none'>
+                  <li><Link href='#' className='text-[#ffffffb3] hover:text-white transition-colors duration-300'>Privacy Policy</Link></li>
+                  <li><Link href='#' className='text-[#ffffffb3] hover:text-white transition-colors duration-300'>Career</Link></li>
+                  <li><Link href='#' className='text-[#ffffffb3] hover:text-white transition-colors duration-300'>Blog</Link></li>
+                  <li><Link href='#' className='text-[#ffffffb3] hover:text-white transition-colors duration-300'>Term & Condition</Link></li>
+                </ul>
+              </div>
+
+              <div className='md:col-span-6 space-y-3 max-w-md'>
+                <h2 className='text-white text-2xl unbounded-font mb-6'>Our Newsletter</h2>
+                <p className='text-[#ffffffb3] text-sm'>
+                  Subscribe to our newsletter for the latest updates and offers.
+                </p>
+                <form className='flex items-center border border-[#ffffff4d] rounded-full mt-4 overflow-hidden'>
+                  <input type="email" placeholder='Your Email Address' className='flex-1 px-4 py-3 text-sm bg-transparent text-white placeholder-[#ffffff80] font-semibold' />
+                  <button className='bg-white text-black font-semibold xl:px-6 py-3 hover:bg-gray-200 md:px-2'>SIGN UP</button>
+                </form>
               </div>
             </div>
           </div>
